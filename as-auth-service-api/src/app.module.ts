be542404e6217279056d61692851f1e459b0bab3
dkarp <dkarputs@gmail.com>
2024-08-22 07:15:08 +0200

    Initial commit

--- as-auth-service-api/src/app.module.ts
+++ as-auth-service-api/src/app.module.ts
@@ -1,10 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from './configs/config.module';
+import { PostgresConfig } from './configs/postgres.config';
+import * as cors from 'cors';
+import { ConfigService } from './configs/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useExisting: PostgresConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  private readonly ORIGIN_URL: string;
+  // constructor(configService: ConfigService) {
+  //   this.ORIGIN_URL = configService.getString('ORIGIN_URL');
+  // }
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer
+  //     .apply(
+  //       cors({
+  //         origin: this.ORIGIN_URL, // Замініть на ваш домен
+  //         credentials: true, // Дозволяє передачу облікових даних
+  //       }),
+  //     )
+  //     .forRoutes('*');
+  // }
+}
